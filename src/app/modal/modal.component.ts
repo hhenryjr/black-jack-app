9c261c7ef3bda72e7286c233124445ab9f685791
hhenryjr <h2rnng@yahoo.com>
2022-08-18 15:14:26 -0700

    refactored styling

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -5,7 +5,7 @@ import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  // styleUrls: ['./modal.component.css']
 })
 
 export class ModalComponent implements OnInit {
